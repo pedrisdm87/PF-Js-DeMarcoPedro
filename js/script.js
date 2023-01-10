34526dd47c7f77e40b1bfa4d21931dd854fd454d
pedrisdm87 <bordyestamp@gmail.com>
2023-01-10 09:57:40 -0300

    agregando pre-entrega 1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,48 @@
-let entrada = prompt("Ingresar una letra");
-let salida  = entrada + " " + "ingresada";
-alert(salida);
+let nombre = "";
+let cantidadProductos=0
+let listaDeProductos=" "
+let total=0
+let iva=1.21
+
+
+
+//IVA
+function agregaIva(total,iva){
+    
+    return (total * iva)
+}
+
+
+
+
+console.log("Hola, bienvenido al Carrito de compras");
+
+usuario = prompt("Ingrese su nombre de Usuario")
+console.log("Usuario INGRESADO:"+" "+usuario)
+
+if(usuario == ""){
+    alert("INGRESE UN USUARIO VALIDO")
+}else{
+    alert("USUARIO INGRESADO:"+" "+usuario)
+}
+
+cantidadProductos=Number(prompt("Ingrese cantidad de productos"))
+
+for (let i= 0; i<cantidadProductos; i++) {
+    let producto=prompt("ingrese el NOMBRE del producto o ESC para CANCELAR")
+    if (producto=="ESC") {
+    break
+    }
+    let valor=Number(prompt("ingrese el VALOR del producto"))
+    total=total+valor
+    listaDeProductos=listaDeProductos +" "+ producto + ","
+
+}
+
+
+
+console.log("Los productos comprados por el usuario :" + usuario + " fueron: ", listaDeProductos)
+console.log("El monto total sin IVA es ", total) 
+console.log("El Total con IVA es: ", agregaIva(total,iva))
+
+
