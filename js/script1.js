de1ab887f1a3c5712ab047443c4ebd85b4605817
pedrisdm87 <bordyestamp@gmail.com>
2023-02-22 09:46:17 -0300

    agregando mas productos

--- js/script1.js
+++ js/script1.js
@@ -34,16 +34,49 @@ class Producto {
 
 //CREO LOS PRODUCTOS Y LOS ALMACENO EN UN ARRAY
 
-const producto1 = new Producto(1, 'Tubo', 300000, 1,'img/tubo.webp');
-const producto2 = new Producto(2, 'Controladora', 220000, 1, 'img/controladora.jpg');
-const producto3 = new Producto(3, 'Lente', 15000, 1, 'img/lente.jpg');
-const producto4 = new Producto(4, 'Fuente', 125, 1,'img/fuente.webp');
+const producto1 = new Producto(1, 'Tubo 100W', 300000, 1,'img/tubo.webp');
+const producto2 = new Producto(2, 'Controladora 6445', 220000, 1, 'img/controladora.jpg');
+const producto3 = new Producto(3, 'Lente D.Focal 101mm', 15000, 1, 'img/lente.jpg');
+const producto4 = new Producto(4, 'Fuente 100w', 185000, 1,'img/fuente.webp');
+const producto5 = new Producto(5, 'Tubo 90W', 200000, 1,'img/tubo.webp');
+const producto6 = new Producto(6, 'Controladora 6442', 220000, 1, 'img/controladora 2.jpg');
+const producto7 = new Producto(7, 'Lente D.Focal 76mm', 15000, 1, 'img/lente.jpg');
+const producto8 = new Producto(8, 'Fuente 90w', 150000, 1,'img/fuente.webp');
+const producto9 = new Producto(9, 'Tubo 80W', 180000, 1,'img/tubo.webp');
+const producto10 = new Producto(10, 'Puntero Láser', 220000, 1, 'img/puntero.jpg');
+const producto11 = new Producto(11, 'Lente D.Focal 63mm', 15000, 1, 'img/lente.jpg');
+const producto12 = new Producto(12, 'Fuente 80w', 125000, 1,'img/fuente.webp');
+const producto13 = new Producto(13, 'Fuente 50w', 75000, 1,'img/fuente.webp');
+const producto14 = new Producto(14, 'Tubo 65W', 110000, 1,'img/tubo.webp');
+const producto15 = new Producto(15, 'Correa 3M', 3500, 1, 'img/correa.webp');
+const producto16 = new Producto(16, 'Lente D.Focal 50mm', 15000, 1, 'img/lente.jpg');
+const producto17 = new Producto(17, 'Fuente 65w', 80000, 1,'img/fuente.webp');
+const producto18 = new Producto(18, 'Tubo 50W', 100000, 1,'img/tubo.webp');
+const producto19 = new Producto(19, 'Polea', 1000, 1, 'img/polea.jpg');
+const producto20 = new Producto(20, 'Lente D.Focal 30mm', 15000, 1, 'img/lente.jpg');
 
-const productos = [producto1, producto2, producto3, producto4];
+const productos = [producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8, producto9, producto10, producto11, producto12, producto13, producto14, producto15, producto16, producto17, producto18, producto19, producto20];
+
+
+
+//OBTENGO LOS DATOS DESDE JSON
+
+async function getDatos() {
+
+  const resp = await fetch('/json/data.json')
+    .then((res) => res.json())
+    .then((data) => {
+      return data;
+    });
+  return resp;
+}
+ 
+const productosjson = getDatos()
+
 
 //MUESTRO LOS PRODUCTOS MODIFICANDO EL DOM
 
-const contenedorProductos = document.getElementById('contenedorProductos');
+let contenedorProductos = document.getElementById('contenedorProductos');
 
 productos.forEach((producto) => {
   const divProducto = document.createElement('div');
@@ -53,7 +86,7 @@ productos.forEach((producto) => {
                               <img src="${producto.imagen}" class="card-img-top img-fluid py-3"> 
                               <div class="card-body">
                                   <h3 class="card-title"> ${producto.nombre} </h3>
-                                  <p class="card-text"> ${producto.precio} </p>
+                                  <p class="card-text"> $ ${producto.precio} </p>
                                   <button id="boton${producto.id}" class="btn btn-primary"> Agregar al Carrito </button>
                               </div>
                           </div>`;
@@ -95,8 +128,9 @@ function actualizarCarrito() {
   let aux = '';
   carrito.forEach((producto) => {
     aux += `
-              <div class="card col-xl-3 col-md-6 col-sm-12">
+                  <div class="card col-xl-3 col-md-6 col-sm-12">
                   <img src="${producto.imagen}"  class="card-img-top img-fluid py-3">
+                  <h2>  </h2>
                   <div class="card-body">
                       <h3 class="card-title"> ${producto.nombre} </h3>
                       <p class="card-text"> ${producto.precio} </p>
